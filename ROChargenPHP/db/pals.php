0900b9101e0543b22def4850da0ac6071d544d62
Perkka2 <36314461+Perkka2@users.noreply.github.com>
2020-02-12 01:06:56 +0100

    Added ROChargen Package

---
+++ ROChargenPHP/db/pals.php
@@ -0,0 +1,164 @@
+<?php
+
+/**
+* @fileoverview Body palette
+* @author Vincent Thibault (alias KeyWorld - Twitter: @robrowser)
+* @version 1.0.0
+*/
+
+return array(
+
+	0 => "초보자",         // novice
+	1 => "검사",           // Swordman
+	2 => "마법사",         // mage
+	3 => "궁수",         // archer
+	4 => "성직자",       // acolyte
+	5 => "상인",           // merchant
+	6 => "도둑",           // thief
+	7 => "기사",           // Knight
+	8 => "프리스트",     // priest
+	9 => "위저드",         // wizard
+	10 => "제철공",        // Blacksmith
+	11 => "헌터",          // Hunter
+	12 => "어세신", // Assassion
+	13 => "페코페코_기사", // peco knight
+	14 => "크루",          // crusader
+	15 => "몽크",          // monk
+	16 => "세이지",    // sage
+	17 => "로그",          // Rogue
+	18 => "연금술사",    // Alchemist
+	19 => "바드",          // bard
+	20 => "무희",         // dancer
+	21 => "페코페코_크루", // peco crusader
+	22 => "결혼",          // wedding
+	23 => "슈퍼노비스",  // Super novice
+	24 => "건너",          // Gunslinger
+	25 => "닌자",          // Ninja
+	26 => "프리스트",    // xmas
+	27 => "프리스트",    // Summer
+
+	// High
+	4001 => "초보자",      // novice
+	4002 => "검사",        // Swordman
+	4003 => "마법사",      // mage
+	4004 => "궁수",      // archer
+	4005 => "성직자",    // acolyte
+	4006 => "상인",        // merchant
+	4007 => "도둑",        // thief
+
+	// 2
+	4008 => "로드나이트",  // Lord Knight
+	4009 => "하이프리스트", // High Priest
+	4010 => "하이위저드",   // High Wizard
+	4011 => "화이트스미스", // Whitesmith
+	4012 => "스나이퍼",      // Sniper
+	4013 => "어세신크로스", // Assassin Cross
+	4014 => "로드나이트",      // Peco Lord Knight
+	4015 => "팔라딘",        // Paladain
+	4016 => "챔피온",        // Champion
+	4017 => "프로페서",     // Professor
+	4018 => "스토커",      // Stalker
+	4019 => "크리에이터",    // Creator
+	4020 => "크라운",        // Clown
+	4021 => "집시",        // Gypsy
+	4022 => "페코팔라",      // Paladin
+
+	// Baby
+	4023 => "초보자",         // novice
+	4024 => "검사",           // Swordman
+	4025 => "마법사",         // mage
+	4026 => "궁수",         // archer
+	4027 => "성직자",       // acolyte
+	4028 => "상인",           // merchant
+	4029 => "도둑",           // thief
+	4030 => "기사",           // Knight
+	4031 => "프리스트",     // priest
+	4032 => "위저드",         // wizard
+	4033 => "위저드",         // Blacksmith
+	4034 => "헌터",           // Hunter
+	4035 => "어세신", // Assassion
+	4036 => "페코페코_기사",  // peco knight
+	4037 => "크루",           // crusader
+	4038 => "몽크",           // monk
+	4039 => "세이지",     // sage
+	4040 => "로그",           // Rogue
+	4041 => "연금술사",     // Alchemist
+	4042 => "바드",           // bard
+	4043 => "무희",          // dancer
+	4044 => "페코페코_크루",  // peco crusader
+	4045 => "슈퍼노비스",   // Super novice
+
+	4046 => "태권소년",       // Taekwon
+	4047 => "권성",        // Star Gladiator
+	4048 => "권성",         // Flying star gladiator
+	4049 => "소울링커",     // Soul linker
+
+	// 3rd
+	4054 => "룬나이트",      // rune knight
+	4055 => "워록",      // warlock
+	4056 => "레인저",        // ranger
+	4057 => "아크비숍",   // arch bishop
+	4058 => "미케닉",  // mechanic
+	4059 => "길로틴크로스", // guillotine
+	4060 => "룬나이트",      // rune knight
+	4061 => "워록",      // warlock
+	4062 => "레인저",        // ranger
+	4063 => "아크비숍",   // arch bishop
+	4064 => "미케닉",  // mechanic
+	4065 => "길로틴크로스", // guillotine
+	4066 => "로얄가드",          // royal guard
+	4067 => "소서러",       // sorcerer
+	4068 => "민스트럴",          // minstrel
+	4069 => "원더러",          // wanderer
+	4070 => "슈라",          // sura
+	4071 => "제네릭",      // genetic
+	4072 => "쉐도우체이서",        // shadow chaser
+	4073 => "로얄가드",          // royal guard
+	4074 => "소서러",       // sorcerer
+	4075 => "민스트럴",          // minstrel
+	4076 => "원더러",          // wanderer
+	4077 => "슈라",          // sura
+	4078 => "제네릭",      // genetic
+	4079 => "쉐도우체이서",        // shadow chaser
+
+	// 3rd Mount
+	4080 => "룬나이트",        // peco rune knight
+	4081 => "룬나이트",        // peco rune knight
+	4082 => "페코팔라",        // royal guard
+	4083 => "페코팔라",        // royal guard
+	4084 => "레인저",        // ranger
+	4085 => "레인저",        // ranger
+	4086 => "미케닉",  // mechanic CHECK THIS
+	4087 => "미케닉",  // mechanic CHECK THIS
+
+	// 3rd Baby
+	4096 => "룬나이트",      // rune knight
+	4097 => "워록",      // warlock
+	4098 => "레인저",        // ranger
+	4099 => "아크비숍",  // arch bishop
+	4100 => "미케닉",  // mechanic
+	4101 => "길로틴크로스", // guillotine
+	4102 => "로얄가드",          // royal guard
+	4103 => "소서러",       // sorcerer
+	4104 => "민스트럴",          // minstrel
+	4105 => "원더러",          // wanderer
+	4106 => "슈라",          // sura
+	4107 => "제네릭",      // genetic
+	4108 => "쉐도우체이서",        // shadow chaser
+
+	// 3rd Baby mount
+	4109 => "룬나이트",        // peco rune knight
+	4110 => "로얄가드",        // royal guard
+	4111 => "레인저",        // ranger
+	4112 => "미케닉",  // mechanic CHECK THIS
+
+	4190 => "슈퍼노비스",    // Super Novice extend ?
+	4191 => "슈퍼노비스",
+
+	4211 => "kagerou",    //Job_Kagerou	4211
+	4212 => "oboro",    //Job_Oboro	4212
+
+	4215 => "슈퍼노비스",    //Job_Rebellion	4215
+
+	4218 => "슈퍼노비스"    //Job_summoner	4218
+);
